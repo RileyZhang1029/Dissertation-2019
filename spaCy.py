0a7b136b61d8e9dadbdf59894b58730b325e927f
RileyZhang1029 <44319408+RileyZhang1029@users.noreply.github.com>
2019-03-29 07:28:04 +0000

    Create spaCy.py

---
+++ spaCy.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Mar 25 23:48:37 2019
+
+@author: Administrator
+"""
+import spacy
+from spacy import displacy
+from collections import Counter
+from pprint import pprint
+import en_core_web_sm
+nlp = en_core_web_sm.load()
+
+doc = nlp('European authorities fined Google a record $5.1 billion on Wednesday for abusing its power in the mobile phone market and ordered the company to alter its practices')
+pprint([(X.text, X.label_) for X in doc.ents])
